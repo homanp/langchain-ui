ff857a447421f929a633ae9785354656b87688fa
Ismail Pelaseyed <homanp@gmail.com>
2023-03-27 23:12:42 +0200

    Add api endpoint for creating a prompt template

--- app/app/prompt-templates/client-page.js
+++ app/app/prompt-templates/client-page.js
@@ -1,28 +1,65 @@
 "use client";
+import React, { useCallback } from "react";
+import { useRouter } from "next/navigation";
 import {
+  Box,
   Button,
   Center,
+  Container,
+  FormControl,
+  FormHelperText,
+  HStack,
   Icon,
+  Spinner,
   Stack,
   Table,
   TableContainer,
+  Tag,
   Tbody,
+  Textarea,
   Tr,
   Td,
-  HStack,
-  Spinner,
   useColorModeValue,
+  Input,
 } from "@chakra-ui/react";
 import { TbPlus } from "react-icons/tb";
 import { useAsync } from "react-use";
+import { useForm } from "react-hook-form";
 import PageHeader from "@/components/page-header";
 import { SIDEBAR_MENU } from "@/lib/sidebar";
-import { getPrompTemplates } from "@/lib/api";
+import {
+  createPromptTemplate,
+  getPrompTemplates,
+  getPromptVariables,
+} from "@/lib/api";
 
 export default function PromptTemplatesClientPage() {
+  const buttonColorScheme = useColorModeValue("blackAlpha", "whiteAlpha");
+  const buttonBackgroundColor = useColorModeValue("black", "white");
+  const router = useRouter();
   const { loading: isLoading, value: promptTemplates } = useAsync(
     async () => getPrompTemplates,
     [getPrompTemplates]
+  );
+
+  const {
+    formState: { isSubmitting, errors },
+    handleSubmit,
+    register,
+  } = useForm();
+
+  const onSubmit = useCallback(
+    async ({ name, prompt }) => {
+      const payload = {
+        name,
+        prompt,
+        inputs: getPromptVariables(prompt),
+      };
+
+      await createPromptTemplate(payload);
+      router.refresh();
+    },
+    [router]
   );
 
   return (
@@ -34,8 +71,8 @@ export default function PromptTemplatesClientPage() {
         <HStack>
           <Button
             leftIcon={<Icon as={TbPlus} />}
-            colorScheme={useColorModeValue("blackAlpha", "whiteAlpha")}
-            backgroundColor={useColorModeValue("black", "white")}
+            colorScheme={buttonColorScheme}
+            backgroundColor={buttonBackgroundColor}
             size="sm"
           >
             New template
@@ -69,6 +106,51 @@ export default function PromptTemplatesClientPage() {
             </Tbody>
           </Table>
         </TableContainer>
+      )}
+      {!isLoading && promptTemplates.length === 0 && (
+        <Center flex={1}>
+          <Container as="form" onSubmit={handleSubmit(onSubmit)}>
+            <Stack>
+              <FormControl isInvalid={errors?.name}>
+                <Input
+                  size="sm"
+                  placeholder="My custom prompt..."
+                  {...register("name", { required: true })}
+                />
+              </FormControl>
+              <FormControl isInvalid={errors?.prompt}>
+                <Box
+                  borderRadius="md"
+                  borderWidth="1px"
+                  padding={3}
+                  spacing={0}
+                >
+                  <Textarea
+                    minHeight="250px"
+                    variant="unstyled"
+                    fontSize="sm"
+                    {...register("prompt", { required: true })}
+                  />
+                  <HStack justifyContent="flex-end">
+                    <Button
+                      colorScheme={buttonColorScheme}
+                      backgroundColor={buttonBackgroundColor}
+                      type="sumbit"
+                      fontSize="sm"
+                      isLoading={isSubmitting}
+                    >
+                      Save
+                    </Button>
+                  </HStack>
+                </Box>
+                <FormHelperText>
+                  Type <Tag size="sm">{`{{myVariable}}`}</Tag> to insert
+                  variables into your template.
+                </FormHelperText>
+              </FormControl>
+            </Stack>
+          </Container>
+        </Center>
       )}
     </Stack>
   );
--- pages/api/prompt-templates/index.js
+++ pages/api/prompt-templates/index.js
@@ -9,16 +9,33 @@ const promptTemplatesHander = async (request, response) => {
 
   if (request.method === "GET") {
     try {
+      const data = await prismaClient.promptTemplate.findMany({
+        where: {
+          userId: {
+            equals: session.user.id,
+          },
+        },
+      });
+
       response.stats(200).json({
         success: true,
-        data: await prismaClient.promptTemplate.findMany({
-          where: {
-            userId: {
-              equals: session.user.id,
-            },
-          },
-        }),
+        data,
       });
+    } catch (error) {
+      response.status(500).json({ succes: false, error });
+    }
+  }
+
+  if (request.method === "POST") {
+    try {
+      const promptTemplate = await prismaClient.promptTemplate.create({
+        data: {
+          userId: session.user.id,
+          ...request.body,
+        },
+      });
+
+      response.status(200).json({ sucess: true, data: promptTemplate });
     } catch (error) {
       response.status(500).json({ succes: false, error });
     }
