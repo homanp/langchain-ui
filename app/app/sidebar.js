98fa6f2ad76ebbe9698163968da093c7c03b6f82
Ismail Pelaseyed <homanp@gmail.com>
2023-03-27 19:18:39 +0200

    Create `Prisma` authentication schema

--- app/app/sidebar.js
+++ app/app/sidebar.js
@@ -2,15 +2,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useRouter } from "next/navigation";
-import {
-  Box,
-  Button,
-  Icon,
-  Stack,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { SIDEBAR_MENU } from "@/lib/sidebar";
+import { Box, Button, Icon, Stack, useColorMode } from "@chakra-ui/react";
+import { useSidebar } from "@/lib/sidebar";
 import UserMenu from "@/components/user-menu";
 
 function SidebarItem({ id, href, label, icon, ...properties }) {
@@ -40,7 +33,8 @@ SidebarItem.propTypes = {
 };
 
 export default function Sidebar() {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const menu = useSidebar();
+  const { colorMode } = useColorMode();
   const isLight = colorMode === "light";
 
   return (
@@ -54,32 +48,35 @@ export default function Sidebar() {
       <Stack spacing={4}>
         <UserMenu padding={2} />
         <Box paddingX={1}>
-          {SIDEBAR_MENU.filter(({ placement }) => placement === "top").map(
-            ({ id, label, href, icon, iconDark }) => (
+          {menu
+            .filter(({ placement }) => placement === "top")
+            .map(({ id, label, href, icon, iconDark, ...properties }) => (
               <SidebarItem
                 key={id}
                 id={id}
                 label={label}
                 icon={isLight ? icon : iconDark || icon}
                 href={href}
+                {...properties}
               />
-            )
-          )}
+            ))}
         </Box>
       </Stack>
       <Stack paddingX={1} spacing={0}>
-        {SIDEBAR_MENU.filter(({ placement }) => placement === "bottom").map(
-          ({ id, label, labelDark, href, icon, iconDark }) => (
-            <SidebarItem
-              key={id}
-              id={id}
-              label={isLight ? label : labelDark || label}
-              icon={isLight ? icon : iconDark || icon}
-              href={href}
-              onClick={id === "dark_mode" && toggleColorMode}
-            />
-          )
-        )}
+        {menu
+          .filter(({ placement }) => placement === "bottom")
+          .map(
+            ({ id, label, labelDark, href, icon, iconDark, ...properties }) => (
+              <SidebarItem
+                key={id}
+                id={id}
+                label={isLight ? label : labelDark || label}
+                icon={isLight ? icon : iconDark || icon}
+                href={href}
+                {...properties}
+              />
+            )
+          )}
       </Stack>
     </Stack>
   );
