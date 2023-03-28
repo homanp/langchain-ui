acebca2387f95ae043eecda7c0bbb0576e4df13b
Ismail Pelaseyed <homanp@gmail.com>
2023-03-28 23:30:18 +0200

    Add basic landing page

--- app/landing-page.js
+++ app/landing-page.js
@@ -9,6 +9,7 @@ import {
   Icon,
   Link,
   Stack,
+  Text,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { TbBrandGithub, TbLink } from "react-icons/tb";
@@ -17,9 +18,10 @@ import { signIn } from "next-auth/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function LandingPage() {
-  const backgroundColor = useColorModeValue("white", "#131416");
+  const backgroundColor = useColorModeValue("#131416", "#131416");
   const buttonColorScheme = useColorModeValue("blackAlpha", "whiteAlpha");
   const buttonBackgroundColor = useColorModeValue("black", "white");
+  const fontColor = useColorModeValue("white", "white");
 
   return (
     <Flex
@@ -28,14 +30,57 @@ export default function LandingPage() {
       className={inter.className}
     >
       <Container maxWidth="7xl">
+        <HStack paddingY={6} justifyContent="space-between">
+          <Text as="strong" color={fontColor} fontSize="lg">
+            LangChain
+            <Text
+              as="span"
+              bgGradient="linear(to-l, #20BDFF, #85D8CE)"
+              backgroundClip="text"
+            >
+              UI
+            </Text>
+          </Text>
+          <HStack spacing={4}>
+            <Link
+              href="https://github.com/homanp/langchain-ui"
+              color={fontColor}
+            >
+              Github
+            </Link>
+            <Link
+              color={fontColor}
+              href="https://github.com/homanp/langchain-ui/blob/main/.github/CONTRIBUTING.md"
+            >
+              Contribute
+            </Link>
+            <Link
+              color={fontColor}
+              href="https://github.com/homanp/langchain-ui#getting-started"
+            >
+              Docs
+            </Link>
+          </HStack>
+        </HStack>
         <Stack
           alignItems="center"
           justifyContent="center"
           paddingY="200px"
           spacing={8}
         >
-          <Heading fontSize="4xl" fontWeight={700} textAlign="center">
-            The open source chat-ai toolkit
+          <Heading
+            fontSize="7xl"
+            fontWeight={700}
+            textAlign="center"
+            color={fontColor}
+          >
+            The open source{" "}
+            <Text
+              bgGradient="linear(to-l, #20BDFF, #26D0CE)"
+              backgroundClip="text"
+            >
+              chat-ai toolkit
+            </Text>
           </Heading>
           <Stack>
             <Button
@@ -55,11 +100,14 @@ export default function LandingPage() {
                 target="_blank"
                 color="gray.500"
               >
-                homanp/langchain-ui
+                Contribute on Github
               </Link>
             </HStack>
           </Stack>
         </Stack>
+        <Container>
+          <Stack></Stack>
+        </Container>
       </Container>
     </Flex>
   );
