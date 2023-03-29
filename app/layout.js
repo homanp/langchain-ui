03721756dc541f31bdaba1e8d928780892df6c7b
Ismail Pelaseyed <homanp@gmail.com>
2023-03-29 08:57:38 +0200

    Enable Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
-import Providers from "./providers";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
