c6472589b2ee4964843c20913cf00b1e22b08758
Ismail Pelaseyed <homanp@gmail.com>
2023-04-09 19:25:10 +0200

    Small UI tweaks (#52)

--- app/providers.js
+++ app/providers.js
@@ -2,11 +2,12 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
+import theme from "@/lib/theme";
 
 export default function Providers({ children }) {
   return (
     <CacheProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <SessionProvider>{children}</SessionProvider>
       </ChakraProvider>
     </CacheProvider>
