c3429bec43f7bf9e4e1cc1e64d88ea06f5895237
Ismail Pelaseyed <homanp@gmail.com>
2023-04-12 21:41:58 +0200

    Add support for straming chatbot response

--- app/api/v1/chatbots/[chatbotId]/route.js
+++ app/api/v1/chatbots/[chatbotId]/route.js
@@ -1,17 +1,6 @@
 import { NextResponse } from "next/server";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { ConversationChain } from "langchain/chains";
-import { CallbackManager } from "langchain/callbacks";
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  SystemMessagePromptTemplate,
-} from "langchain/prompts";
-import { MessagesPlaceholder } from "langchain/prompts";
-import { BufferMemory, ChatMessageHistory } from "langchain/memory";
-import { HumanChatMessage, AIChatMessage } from "langchain/schema";
-import { DEFAULT_PROMPT_TEMPLATE } from "@/lib/prompt-template";
 import { prismaClient } from "@/lib/prisma";
+import { useChain } from "@/lib/chain";
 
 export const runtime = "nodejs";
 
@@ -42,47 +31,28 @@ export async function POST(request, { params }) {
       })
     : undefined;
 
-  const history = messages.map(({ agent, message }) =>
-    agent === "ai" ? new AIChatMessage(message) : new HumanChatMessage(message)
-  );
+  const handleNewToken = async (token) => {
+    await writer.ready;
+    await writer.write(encoder.encode(`data: ${token}\n\n`));
+  };
 
-  const memory = new BufferMemory({
-    memoryKey: "history",
-    chatHistory: new ChatMessageHistory(history),
-    returnMessages: true,
-  });
+  const handleTokenEnd = async () => {
+    await writer.ready;
+    await writer.write(encoder.encode(`data: CLOSE\n\n`));
+    await writer.close();
+  };
 
-  const llm = new ChatOpenAI({
-    temperature: 0,
-    streaming: true,
-    callbackManager: CallbackManager.fromHandlers({
-      handleLLMNewToken: async (token) => {
-        await writer.ready;
-        await writer.write(encoder.encode(`data: ${token}\n\n`));
-      },
-      handleLLMEnd: async () => {
-        await writer.ready;
-        await writer.close();
-      },
-      handleLLMError: async (e) => {
-        await writer.ready;
-        await writer.abort(e);
-      },
-    }),
-  });
+  const handleTokenError = async (error) => {
+    await writer.ready;
+    await writer.abort(error);
+  };
 
-  const prompt = ChatPromptTemplate.fromPromptMessages([
-    SystemMessagePromptTemplate.fromTemplate(
-      promptTemplate?.prompt || DEFAULT_PROMPT_TEMPLATE
-    ),
-    new MessagesPlaceholder("history"),
-    HumanMessagePromptTemplate.fromTemplate("{message}"),
-  ]);
-
-  const chain = new ConversationChain({
-    memory,
-    prompt,
-    llm,
+  const chain = useChain({
+    messages,
+    promptTemplate,
+    onLLMNewToken: handleNewToken,
+    onLLMEnd: handleTokenEnd,
+    onLLMError: handleTokenError,
   });
 
   chain.call({ message });
