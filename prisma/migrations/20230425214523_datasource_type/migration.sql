1369c666ec2a6631f17c835f698ba34b6518026f
Ismail Pelaseyed <homanp@gmail.com>
2023-04-28 13:49:23 +0200

    Refactor migraitons

--- prisma/migrations/20230425214523_datasource_type/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `type` to the `Datasource` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Datasource" ADD COLUMN     "type" TEXT NOT NULL;
